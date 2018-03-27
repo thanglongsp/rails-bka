74bff59ed4a439ae2f2a57ab033e0c598096a55b
thănglongsp <thanglongnguyensp@gmail.com>
2018-03-27 20:51:09 +0000

    completed signup, login, logout ...

---
+++ app/views/images/_image.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! image, :id, :user_id, :title, :ﬁle, :created_at, :created_at, :updated_at
+json.url image_url(image, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
+  root 'static_pages#home'
+  get 'static_pages/home'
+
+  devise_for :users
+  resources :images
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
